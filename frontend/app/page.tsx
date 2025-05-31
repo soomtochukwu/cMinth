16e38ac85928cf3454d891106fc334a284d383f4
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2025-05-31 09:03:54 +0100

    initialization

---
+++ frontend/app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import "./globals.css";
+
+const page = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className=" flex flex-col items-center">
+        <h1 className="text-3xl font-bold ">Welcome to Cr8or 🎭!</h1>
+        <span className="text-sm">
+          ...everything is still in template format
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default page;
