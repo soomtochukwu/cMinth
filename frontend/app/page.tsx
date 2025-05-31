de5e713200591de24f69704502844d8950c3bac5
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2025-05-31 09:15:12 +0100

    Feat: :)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,9 @@
 "use client";
-import "./globals.css";
+
+import Welcome from "@/components/Welcome";
 
 const page = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className=" flex flex-col items-center">
-        <h1 className="text-3xl font-bold ">Welcome to Cr8or 🎭!</h1>
-        <span className="text-sm">
-          ...everything is still in template format
-        </span>
-      </div>
-    </div>
-  );
+  return <Welcome />;
 };
 
 export default page;
---
+++ frontend/components/Welcome.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className=" flex flex-col items-center">
+        <h1 className="text-3xl font-bold ">Welcome to Cr8or 🎭!</h1>
+        <span className="text-sm">
+          ...everything is still in template format
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
